09fad00cbe19ebaf13d4acda3529ce657f009449
Alexandre Saint <830589+asnt@users.noreply.github.com>
2019-11-24 18:34:34 +0100

    Fix (tentative): Handle different input/output image sizes

--- show.m
+++ show.m
@@ -21,8 +21,16 @@ for i = 1 : numel(output_dir)
     img_fn = output_dir(i).name(1:end-4);
     img_fn = strrep(img_fn, '_blob_0', '');
     img = imread(fullfile(data_root_folder, 'images', [img_fn, '.jpg']));
-    img_row = size(img, 1);
-    img_col = size(img, 2);
+
+    % img_row = size(img, 1);
+    % img_col = size(img, 2);
+
+    % XXX: Verify that this makes sense.
+    % Use minimum size between labelled image and input image.
+    img_size = min(size(img)(1:2), size(raw_result));
+    img_row = img_size(1);
+    img_col = img_size(2);
+
     result = raw_result(1:img_row, 1:img_col);
     mask = uint8(result);
     imwrite(mask, colormap, fullfile(save_result_folder, [img_fn, '.png']));
